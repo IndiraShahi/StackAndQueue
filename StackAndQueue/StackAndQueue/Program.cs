d1f29417556e001fb0ecba9ae024bd887bdda87c
IndiraShahi <IndiraShahi144@gmail.com>
2021-06-15 06:23:50 +0530

    [Indira] ADDED: Create a stack and add values

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-
 namespace StackAndQueue
-{
-    class Program
     {
-        static void Main(string[] args)
+        public class Program
         {
-            Console.WriteLine("Hello World!");
+        public static void Main(String[] args)
+        {
+            // create Object of Implementing class
+            StackUsingLinkedlist obj = new StackUsingLinkedlist();
+
+             // insert Stack value
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+
+            // print Stack elements
+            obj.display();
+
+            // print Top element of Stack
+            Console.Write("\nTop element is {0}\n", obj.peek());
+            // Delete top element of Stack
+            obj.pop();
+            obj.pop();
+            obj.pop();
+
+
+            // print Stack elements
+            obj.display();
+
+            // print Top element of Stack
+            Console.Write("\nTop element is {0}\n", obj.peek());
         }
     }
 }
